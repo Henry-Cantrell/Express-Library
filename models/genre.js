d6a02fd5eac0b937ec6631693889a064c66e1986
Henry <hencan.1997@gmail.com>
2021-07-12 20:26:37 -0400

    Schemas created

---
+++ models/genre.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema(
+  {
+    name: {type: String, required: true, min: 3, max: 20},
+  }
+);
+
+// Virtual for genre's URL
+GenreSchema
+.virtual('url')
+.get(function () {
+  return '/genre' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Genre', GenreSchema);
+
